e38bb40f730241cdc82764714770a1b96cb94964
Christian Ossa <christian.ossa@sansano.usm.cl>
2022-07-04 23:49:02 -0400

    Updates

---
+++ precache-manifest.c33402f5cc72dd7c6fa686f6c98073e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24b597a4002f5e8dac12",
+    "url": "/valtiersystem/static/css/main.f56dda4e.chunk.css"
+  },
+  {
+    "revision": "24b597a4002f5e8dac12",
+    "url": "/valtiersystem/static/js/main.d684e378.chunk.js"
+  },
+  {
+    "revision": "3c2c1be5d89a264147c1",
+    "url": "/valtiersystem/static/js/runtime~main.afd472a1.js"
+  },
+  {
+    "revision": "3f3dfd1062dddfac8d46",
+    "url": "/valtiersystem/static/js/2.690be04c.chunk.js"
+  },
+  {
+    "revision": "59b6c15ce9babadac1f55627f86f9f17",
+    "url": "/valtiersystem/index.html"
+  }
+];
